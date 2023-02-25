c61a16432f86aef6455e6a94fbc202168e470d96
1aerostorm <aerostorm1@gmail.com>
2023-02-25 14:44:25 +0000

    Support get all token prices

--- src/pages/api/v1/cmc/[[...all]].js
+++ src/pages/api/v1/cmc/[[...all]].js
@@ -36,6 +36,15 @@ function getID(sym) {
     return null
 }
 
+function getGolosSym(id) {
+    for (const [cmcSym, val] of Object.entries(cfgSymbols)) {
+        if (parseInt(val[1]) === parseInt(id)) {
+            return val[0]
+        }
+    }
+    return null
+}
+
 const doRequest = async (convert) => {
     let url = new URL('/v2/cryptocurrency/quotes/latest', host)
     url.searchParams.set('id', allIDs)
@@ -54,7 +63,7 @@ const doRequest = async (convert) => {
 let handler = nc({ attachParams: true, })
     .use(corsMiddleware())
 
-    .get('/api/v1/cmc/:sym', async (req, res) => {
+    .get('/api/v1/cmc/:sym?', async (req, res) => {
         if (!cfg || !cfg.api_key) {
             let key = cfgKey
             if (!cfg.api_key) key += '.api_key'
@@ -67,26 +76,29 @@ let handler = nc({ attachParams: true, })
             return
         }
 
-        const id = getID(req.params.sym)
+        let id
+        if (req.params.sym) {
+            id = getID(req.params.sym)
 
-        if (!id) {
-            const assets = await golos.api.getAssetsAsync('',  [req.params.sym])
-            if (assets.length) {
+            if (!id) {
+                const assets = await golos.api.getAssetsAsync('',  [req.params.sym])
+                if (assets.length) {
+                    res.json({
+                        status: 'err',
+                        error: 'such token is present on Golos Blockchain, but not supported by GOLOS CMC wrapper',
+                        price_usd: null,
+                        price_rub: null
+                    })
+                    return
+                }
                 res.json({
                     status: 'err',
-                    error: 'such token is present on Golos Blockchain, but not supported by GOLOS CMC wrapper',
+                    error: 'no such token on Golos Blockchain',
                     price_usd: null,
                     price_rub: null
                 })
                 return
             }
-            res.json({
-                status: 'err',
-                error: 'no such token on Golos Blockchain',
-                price_usd: null,
-                price_rub: null
-            })
-            return
         }
 
         let resp, updated
@@ -109,9 +121,7 @@ let handler = nc({ attachParams: true, })
             from_cache = true
         }
 
-        const data = resp.data[id]
-
-        const getPrice = (sym) => {
+        const getPrice = (data, sym) => {
             try {
                 return data.quote[sym].price
             } catch (err) {
@@ -119,13 +129,34 @@ let handler = nc({ attachParams: true, })
             }
         }
 
+        if (id) {
+            const data = resp.data[id]
+
+            res.json({
+                status: 'ok',
+                price_usd: getPrice(data, 'USD'),
+                price_rub: getPrice(data, 'RUB'),
+                from_cache,
+                updated: updated.toISOString().split('.')[0],
+                data,
+            })
+            return
+        }
+
+        const dataMap = {}
+
+        for (const [id, data] of Object.entries(resp.data)) {
+            const sym = getGolosSym(id)
+            dataMap[sym] = data
+            dataMap[sym].price_usd = getPrice(data, 'USD')
+            dataMap[sym].price_rub = getPrice(data, 'RUB')
+        }
+
         res.json({
             status: 'ok',
-            price_usd: getPrice('USD'),
-            price_rub: getPrice('RUB'),
             from_cache,
             updated: updated.toISOString().split('.')[0],
-            data,
+            data: dataMap,
         })
     })
 
